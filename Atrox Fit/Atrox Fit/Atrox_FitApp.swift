915ebf400346f01230febf56ede9634975a046cb
Vali <valimikayilov@yahoo.com>
2023-10-28 15:46:55 +0200

    initial files

---
+++ Atrox Fit/Atrox Fit/Atrox_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Atrox_FitApp.swift
+//  Atrox Fit
+//
+//  Created by vali on 28.10.23.
+//
+
+import SwiftUI
+
+@main
+struct Atrox_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Atrox Fit/Atrox Fit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Atrox Fit
+//
+//  Created by vali on 28.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
